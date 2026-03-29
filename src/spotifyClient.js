baa20c822e1937b8ca48b861b91c3b19f7070181
MossIconSF <mossicon@live.com>
2026-03-28 20:45:57 -0700

    Refactor Spotify client initialization
    Updated Spotify client configuration to use new environment variables and a specific redirect URI.

--- src/spotifyClient.js
+++ src/spotifyClient.js
@@ -1,12 +1,9 @@
 import SpotifyWebApi from "spotify-web-api-node";
 
-export const createSpotifyClient = () =>
-  new SpotifyWebApi({
-    clientId: process.env.CLIENT_ID,
-    clientSecret: process.env.CLIENT_SECRET,
-    redirectUri: process.env.REDIRECT_URI
+export const createSpotifyClient = () => {
+  return new SpotifyWebApi({
+    clientId: process.env.SPOTIFY_CLIENT_ID,
+    clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+    redirectUri: "https://spotify-artist-bot.onrender.com/auth/callback"
   });
-
-// Default client instance (used by artist.js)
-const spotifyApi = createSpotifyClient();
-export default spotifyApi;
+};
